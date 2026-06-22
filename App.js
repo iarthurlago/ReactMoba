a76af5e9eed643c0107c7bab3b92074a117f1c1c
iarthurlago <iarthur.almeida@gmail.com>
2026-06-22 11:32:51 -0300

    building accelerometet

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.Text}>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [leitura, setleitura] = useState({x: 0, y: 0, z: 0})
+
+  useEffect(() => {
+    // define quantos milissegundos entre cada leitura
+    // 100ms = 10 leituras por segundo - fluido e econômico para bateria
+    Accelerometer.setUpdateInterval(50)
+    // inscreve: o sensor chama setLeitura toda vez que o valor muda
+    const inscricao = Accelerometer.addListener(setleitura) //constroi uma ponte entre voce e o componente de comunicaçao entre voce e...
+    // cleanup: cancela a inscrição quando o componente sai da tela
+    // sem isso o listener continua rodando mesmo invicível
+    return inscricao.remove()
+  })
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +28,4 @@ const styles = StyleSheet.create({
     fontSize: 50
   }
 });
+}
